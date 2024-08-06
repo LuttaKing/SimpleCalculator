359a3876f1589038861487d175b3cbd15e7dff77
LuttaKing <denilsonwashuma@gmail.com>
2024-08-07 00:18:07 +0300

    Initial Commit

---
+++ SimpleCalculator/SimpleCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalculatorApp.swift
+//  SimpleCalculator
+//
+//  Created by Denilson Washuma on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
